10479af3bf54d161a0b263d1671b11f8ecb58149
William Chow <431741580derp@gmail.com>
2017-01-18 20:47:56 +0800

    Added more code, Elements Bootloader

--- linkCard.js
+++ linkCard.js
@@ -1,29 +1,54 @@
 'use strict'
 
-let ElementsCardLinkHolderProto = Object.create(HTMLElement.prototype);
+// Elements Bootloader
+if (!Elements) {
+	window.Elements = {}
+}
 
-ElementsCardLinkHolderProto.createdCallback = function () {
+Elements.checkInitProperty = (object, property, default = '') => {
+	if (object[property] !== undefined && object[property] !== '') {
+		return object[property];
+	} else {
+		return default;
+	}
+};
+
+Elements.CardLinkHolderProto = Object.create(HTMLElement.prototype);
+
+Elements.CardLinkHolderProto.createdCallback = function () {
       let shadow = this.createShadowRoot();
       let template = document.importNode(
          document.querySelector('#templateElementsCardLinkHolder'),
          true);
+		shadow.appendChild(template.content)
 }
 
-let ElementsCardLinkHolder = document.registerElement('Elements-CardLinkHolder', {
-    prototype: ElementsCardLinkHolderProto
+let Elements.CardLinkHolder = document.registerElement('Elements-CardLinkHolder', {
+    prototype: Elements.CardLinkHolderProto
 });
 
 
 
-let ElementsCardLinkProto = Object.create(HTMLElement.prototype);
+let Elements.CardLinkProto = Object.create(HTMLElement.prototype);
 
-ElementsCardLinkProto.createdCallback = function () {
+Elements.CardLinkProto.createdCallback = function () {
       let shadow = this.createShadowRoot();
       let template = document.importNode(
          document.querySelector('#templateElementsLinkCard'),
          true);
-}
 
-let ElementsCardLink = document.registerElement('Elements-LinkCard', {
-    prototype: ElementsCardLinkProto
+		let imgPath = Elements.checkInitProperty(this, 'src', '');
+
+		let title = Elements.checkInitProperty(this, 'title', '');
+
+		let faviconImg = template.content.querySelector('#faviconImg');
+		let thumbImg = template.content.querySelector('#thumbImg');
+		let titleSpan = template.content.querySelector('#titleSpan');
+
+		
+		shadow.appendChild(template.content)
+};
+
+let Elements.CardLink = document.registerElement('Elements-LinkCard', {
+    prototype: Elements.CardLinkProto
 });
