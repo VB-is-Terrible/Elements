aaa08a1d1917540725e1594f685cf44fcd74a7c3
William Chow <431741580derp@gmail.com>
2017-01-19 09:04:23 +0800

    Got code working, now to mess with CSS

--- linkCard.js
+++ linkCard.js
@@ -1,54 +1,117 @@
 'use strict'
 
 // Elements Bootloader
-if (!Elements) {
+if (!("Elements" in window)) {
 	window.Elements = {}
 }
 
-Elements.checkInitProperty = (object, property, default = '') => {
+Elements.checkInitProperty = (object, property, value = '') => {
 	if (object[property] !== undefined && object[property] !== '') {
 		return object[property];
+	} else if (object.getAttribute(property) !== null) {
+		return object.getAttribute(property);
 	} else {
-		return default;
+		return value;
 	}
 };
+
+/* Sets up hidden property, setters, getters, hooks to attribute modifications
+ * Takes care of standard plumbing required to make HTMLElement like properties
+ */
+Elements.setUpAttrPropertyLink = (object, property, inital=null, extra = ()=>{}) => {
+	let hidden;
+	let getter = () => {return hidden;};
+	let setter = (value) => {
+		hidden = value;
+		object.setAttribute(property, value);
+		extra(value);
+	};
+
+	Object.defineProperty(object, property, {
+		enumerable: true,
+		configurable: true,
+		get: getter,
+		set: setter
+	});
+
+	object.getDict[property] = getter;
+	object.setDict[property] = setter;
+
+	setter(inital);
+
+	return {
+		get: getter,
+		set: setter
+	};
+};
+
 
 Elements.CardLinkHolderProto = Object.create(HTMLElement.prototype);
 
 Elements.CardLinkHolderProto.createdCallback = function () {
-      let shadow = this.createShadowRoot();
-      let template = document.importNode(
-         document.querySelector('#templateElementsCardLinkHolder'),
-         true);
-		shadow.appendChild(template.content)
+   let shadow = this.createShadowRoot();
+   let template = document.importNode(
+      document.querySelector('#templateElementsCardLinkHolder'),
+      true);
+	shadow.appendChild(template.content)
 }
 
-let Elements.CardLinkHolder = document.registerElement('Elements-CardLinkHolder', {
+Elements.CardLinkHolder = document.registerElement('Elements-CardLinkHolder', {
     prototype: Elements.CardLinkHolderProto
 });
 
 
 
-let Elements.CardLinkProto = Object.create(HTMLElement.prototype);
+Elements.CardLinkProto = Object.create(HTMLElement.prototype);
 
 Elements.CardLinkProto.createdCallback = function () {
-      let shadow = this.createShadowRoot();
-      let template = document.importNode(
-         document.querySelector('#templateElementsLinkCard'),
-         true);
+	let shadow = this.createShadowRoot();
+	let template = document.importNode(
+	   document.querySelector('#templateElementsLinkCard'),
+	   true);
 
-		let imgPath = Elements.checkInitProperty(this, 'src', '');
+	this.getDict = {};
+	this.setDict = {};
 
-		let title = Elements.checkInitProperty(this, 'title', '');
+	let imgPath = Elements.checkInitProperty(this, 'src', '');
+	let title = Elements.checkInitProperty(this, 'title', '');
+	let link = Elements.checkInitProperty(this, 'href', 'about:blank')
 
-		let faviconImg = template.content.querySelector('#faviconImg');
-		let thumbImg = template.content.querySelector('#thumbImg');
-		let titleSpan = template.content.querySelector('#titleSpan');
+	let faviconImg = template.content.querySelector('#faviconImg');
+	let thumbImg = template.content.querySelector('#thumbImg');
+	let titleSpan = template.content.querySelector('#titleSpan');
+	let linkElem = template.content.querySelector('#CardLink');
 
-		
-		shadow.appendChild(template.content)
+
+	let imgChange = (value) => {
+		faviconImg.src = value;
+		thumbImg.src = value;
+	};
+
+	let titleChange = (value) => {
+		if (!(value === null || value === undefined)) {
+			titleSpan.textContent = value;
+		}
+	};
+
+	let linkChange = (value) => {
+		linkElem.href = value;
+	};
+
+	Elements.setUpAttrPropertyLink(this, 'src', imgPath, imgChange);
+	Elements.setUpAttrPropertyLink(this, 'title', title, titleChange);
+	Elements.setUpAttrPropertyLink(this, 'href', link, linkChange);
+
+	shadow.appendChild(template.content)
 };
 
-let Elements.CardLink = document.registerElement('Elements-LinkCard', {
+Elements.CardLinkProto.attributeChangedCallback = function (
+	attrName, oldValue, newValue) {
+	if (attrName in this.setDict) {
+		this.setDict[attrName](newValue);
+	}
+};
+
+Elements.CardLink = document.registerElement('Elements-LinkCard', {
     prototype: Elements.CardLinkProto
 });
