806718245fd6de6973326f478e89106e7c09185a
William Chow <431741580derp@gmail.com>
2017-01-20 03:22:48 +0800

    Thumb and icon now treated seperatly. Thumb and icon still refer to both if one is unspecified Slight UI changes

--- linkCard.js
+++ linkCard.js
@@ -73,18 +73,38 @@ Elements.CardLinkProto.createdCallback = function () {
 	this.getDict = {};
 	this.setDict = {};
 
-	let imgPath = Elements.checkInitProperty(this, 'src', '');
+	let faviconSet = false;
+	let thumbSet = false;
+
+	let faviconPath = Elements.checkInitProperty(this, 'favicon', null);
 	let title = Elements.checkInitProperty(this, 'title', '');
-	let link = Elements.checkInitProperty(this, 'href', 'about:blank')
+	let link = Elements.checkInitProperty(this, 'href', 'about:blank');
+	let thumbPath = Elements.checkInitProperty(this, 'src', null);
+
+	if (faviconPath !== null) {
+		faviconSet = true;
+	}
+
+	if (thumbPath !== null) {
+		thumbSet = true;
+	}
+
+	if (faviconSet && !thumbSet) {
+		thumbPath = faviconPath;
+	} else if (!faviconSet && thumbSet) {
+		faviconPath = thumbPath;
+	}
 
 	let faviconImg = template.content.querySelector('#faviconImg');
 	let thumbImg = template.content.querySelector('#thumbImg');
 	let titleSpan = template.content.querySelector('#titleSpan');
 	let linkElem = template.content.querySelector('#CardLink');
 
-
-	let imgChange = (value) => {
+	let faviconChange = (value) => {
 		faviconImg.src = value;
+	};
+
+	let thumbChange = (value) => {
 		thumbImg.src = value;
 	};
 
@@ -98,9 +118,10 @@ Elements.CardLinkProto.createdCallback = function () {
 		linkElem.href = value;
 	};
 
-	Elements.setUpAttrPropertyLink(this, 'src', imgPath, imgChange);
+	Elements.setUpAttrPropertyLink(this, 'favicon', faviconPath, faviconChange);
 	Elements.setUpAttrPropertyLink(this, 'title', title, titleChange);
 	Elements.setUpAttrPropertyLink(this, 'href', link, linkChange);
+	Elements.setUpAttrPropertyLink(this, 'src', thumbPath, thumbChange);
 
 	shadow.appendChild(template.content)
 };
