a9d2c7f01201a669f998dffc8f74891d89416bbf
William Chow <431741580derp@gmail.com>
2017-02-27 23:51:40 +1100

    Switching to backbone

--- linkCard.js
+++ linkCard.js
@@ -1,72 +1,33 @@
 'use strict'
 
-// Elements Bootloader
-if (!("Elements" in window)) {
-	window.Elements = {}
-}
 
-Elements.checkInitProperty = (object, property, value = '') => {
-	if (object[property] !== undefined && object[property] !== '') {
-		return object[property];
-	} else if (object.getAttribute(property) !== null) {
-		return object.getAttribute(property);
-	} else {
-		return value;
+// Elements.LinkCardContainerProto = Object.create(HTMLElement.prototype);
+//
+// Elements.LinkCardContainerProto.createdCallback = function () {
+//    let shadow = this.createShadowRoot();
+//    let template = document.importNode(
+//       document.querySelector('#templateElementsLinkCardContainer'),
+//       true);
+// 	shadow.appendChild(template.content)
+// }
+//
+// Elements.SideLinkBox = document.registerElement('elements-LinkCardContainer', {
+//     prototype: Elements.LinkCardContainerProto
+// });
+
+Elements.elements.LinkCardContainer = class extends Elements.elements.backbone {
+	constructor () {
+		super();
+
+		let shadow = this.createShadowRoot();
+	   let template = document.importNode(
+	      document.querySelector('#templateElementsLinkCardContainer'),
+	      true);
+		shadow.appendChild(template.content)
 	}
 };
 
-/* Sets up hidden property, setters, getters, hooks to attribute modifications
- * Takes care of standard plumbing required to make HTMLElement like properties
- */
-Elements.setUpAttrPropertyLink = (object, property, inital=null, extra = ()=>{}) => {
-	let hidden;
-	let getter = () => {return hidden;};
-	let setter = (value) => {
-		hidden = value;
-		object.setAttribute(property, value);
-		extra(value);
-	};
-
-	Object.defineProperty(object, property, {
-		enumerable: true,
-		configurable: true,
-		get: getter,
-		set: setter
-	});
-
-	object.getDict[property] = getter;
-	object.setDict[property] = setter;
-
-	setter(inital);
-
-	return {
-		get: getter,
-		set: setter
-	};
-};
-
-Elements.addAttributeProperty = (object, property, inital=null, extra = () => {}) => {
-	let value = Elements.checkInitProperty(object, property, inital);
-	Elements.createAttrPropertyLink(object, property, value, extra);
-};
-
-
-
-
-Elements.LinkCardContainerProto = Object.create(HTMLElement.prototype);
-
-Elements.LinkCardContainerProto.createdCallback = function () {
-   let shadow = this.createShadowRoot();
-   let template = document.importNode(
-      document.querySelector('#templateElementsLinkCardContainer'),
-      true);
-	shadow.appendChild(template.content)
-}
-
-Elements.SideLinkBox = document.registerElement('elements-LinkCardContainer', {
-    prototype: Elements.LinkCardContainerProto
-});
-
+window.customElements.define('elements-linkcard-container', Elements.elements.LinkCardContainer);
 
 
 Elements.CardLinkProto = Object.create(HTMLElement.prototype);
@@ -143,6 +104,81 @@ Elements.CardLinkProto.attributeChangedCallback = function (
 Elements.CardLink = document.registerElement('elements-linkcard', {
     prototype: Elements.CardLinkProto
 });
+//
+// Elements.CardLinkProto = class extends HTMLElement {
+// 	constructor () {
+// 		super();
+// 		let shadow = this.createShadowRoot();
+// 		let template = document.importNode(
+// 			document.querySelector('#templateElementsLinkCard'),
+// 			true);
+//
+// 		this.isInit = false;
+//
+// 		this.getDict = {};
+// 		this.setDict = {};
+//
+// 		shadow.appendChild(template.content)
+//
+// 	}
+// 	connectedCallback () {
+//
+// 		let faviconSet = false;
+// 		let thumbSet = false;
+//
+// 		let faviconPath = Elements.checkInitProperty(this, 'favicon', null);
+// 		let title = Elements.checkInitProperty(this, 'title', '');
+// 		let link = Elements.checkInitProperty(this, 'href', 'about:blank');
+// 		let thumbPath = Elements.checkInitProperty(this, 'src', null);
+//
+// 		if (faviconPath !== null) {
+// 			faviconSet = true;
+// 		}
+//
+// 		if (thumbPath !== null) {
+// 			thumbSet = true;
+// 		}
+//
+// 		if (faviconSet && !thumbSet) {
+// 			thumbPath = faviconPath;
+// 		} else if (!faviconSet && thumbSet) {
+// 			faviconPath = thumbPath;
+// 		}
+//
+// 		let faviconImg = template.content.querySelector('#faviconImg');
+// 		let thumbImg = template.content.querySelector('#thumbImg');
+// 		let titleSpan = template.content.querySelector('#titleSpan');
+// 		let linkElem = template.content.querySelector('#CardLink');
+//
+// 		let faviconChange = (value) => {
+// 			faviconImg.src = value;
+// 		};
+//
+// 		let thumbChange = (value) => {
+// 			thumbImg.src = value;
+// 		};
+//
+// 		let titleChange = (value) => {
+// 			if (!(value === null || value === undefined)) {
+// 				titleSpan.textContent = value;
+// 			}
+// 		};
+//
+// 		let linkChange = (value) => {
+// 			linkElem.href = value;
+// 		};
+//
+// 		Elements.setUpAttrPropertyLink(this, 'favicon', faviconPath, faviconChange);
+// 		Elements.setUpAttrPropertyLink(this, 'title', title, titleChange);
+// 		Elements.setUpAttrPropertyLink(this, 'href', link, linkChange);
+// 		Elements.setUpAttrPropertyLink(this, 'src', thumbPath, thumbChange);
+//
+// 	}
+// }
+//
+//
+
+
 
 Elements.LinkCardHolder = class extends HTMLElement {
 	constructor () {
