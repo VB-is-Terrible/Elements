ab0cd726899626da11257757442c414afd14fbc5
William Chow <431741580derp@gmail.com>
2017-08-13 04:06:52 +1000

    bugfixes for element generator

--- createElement.py
+++ createElement.py
@@ -7,7 +7,7 @@ import os
 def removeDashes(s):
     l = [x.capitalize() for x in s.split('-')]
     n1 = ''.join(l)
-    l[0] = l[0][:1].lower() + l[0][:1]
+    l[0] = l[0][:1].lower() + l[0][1:]
     n0 = ''.join(l)
     return n0, n1
 
@@ -28,7 +28,7 @@ def copy(fin, fout, n0, n1, n2):
         line = line.replace('$0', n0)
         line = line.replace('$1', n1)
         line = line.replace('$2', n2)
-    fout.write(line)
+        fout.write(line)
     fin.close()
     fout.close()
 
@@ -42,6 +42,20 @@ def main():
     if checkExists(name0):
         print('File already exists')
         return
-    copy('Templates/nameTemplate.html', name0 + 'Template.html', name0, name1, name2)
-    copy('Templates/tempalte.css', name0 + '.css', name0, name1, name2)
-    copy('Templates/tempalte.js', name0 + '.js', name0, name1, name2)
+    print(name0, name1, name2)
+    try:
+        copy('Templates/nameTemplate.html', name0 + 'Template.html', name0, name1, name2)
+        copy('Templates/template.css', name0 + '.css', name0, name1, name2)
+        copy('Templates/template.js', name0 + '.js', name0, name1, name2)
+    except Exception as e:
+        print('The following error occured during copying')
+        print(e)
+        l = [name0 + suffix for suffix in ['Template.html', '.css', '.js']]
+        for name in l:
+            try:
+                os.remove(name)
+            except:
+                pass
+
+if __name__ == '__main__':
+    main()
