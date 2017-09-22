06167b6a2e428ec617e3c5c39c0abdcb443a0e9f
William Chow <431741580derp@gmail.com>
2017-09-22 14:27:55 +1000

    Kerbal Panel is a thing!

---
+++ kerbalPanelMenu.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Maximise/Minimise event
+ * @event Elements.elements.KerbalPanelMenu#maximise
+ * @type {Object}
+ * @property {String} detail Name of the thing to maximise/minimise
+ */
+/**
+ * Centre event
+ * @event Elements.elements.KerbalPanelMenu#centre
+ * @type {Object}
+ * @property {String} detail Name of the thing to centre
+ */
+/**
+ * [KerbalPanelMenu description]
+ * @type {Object}
+ * @fires Elements.elements.KerbalPanelMenu#centre
+ * @fires Elements.elements.KerbalPanelMenu#maximise
+ * @augments Elements.elements.backbone
+ */
+Elements.elements.KerbalPanelMenu = class extends Elements.elements.backbone {
+	constructor () {
+		super();
+		const self = this;
+
+		this.name = 'KerbalPanelMenu';
+		let shadow = this.attachShadow({mode: 'open'});
+		let template = Elements.importTemplate(this.name);
+
+		let divs = template.querySelectorAll('div.option');
+		for (let div of divs) {
+			let value = div.getAttribute('value');
+			let detail = {detail: value};
+			let max = div.querySelector('button.max');
+			max.addEventListener('click', (e) => {
+				let event = new CustomEvent('maximise', detail);
+				self.dispatchEvent(event);
+			});
+			let centre = div.querySelector('button.centre');
+			centre.addEventListener('click', (e) => {
+				let event = new CustomEvent('centre', detail);
+				self.dispatchEvent(event);
+			});
+		}
+		//Fancy code goes here
+		shadow.appendChild(template);
+	}
+}
+
+Elements.load('kerbalPanelMenuTemplate.html', Elements.elements.KerbalPanelMenu, 'elements-kerbal-panel-menu');
