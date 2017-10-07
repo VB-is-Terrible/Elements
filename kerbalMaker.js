VB-is-Terrible/Elements

commit 051d447f2d8dd19a5a57b721c836288827057634

Fixed infinte recursion case

kerbalMaker.js
@@ -141,7 +141,7 @@ Elements.elements.KerbalMaker = class extends Elements.elements.dragged {
 		this.data = new KNS.Kerbal();
 		this.data.name = '&nbsp;';
 		this.data.text = defaultJob;
-		this.data.addDisplay(this.elements.kerbal);
+		this.elements.kerbal.data = this.data;
 		this.nameChanged = false;
 		this.nameValid = false;
 		this.elements.nameInput.value = '';

KDB.js
@@ -169,7 +169,6 @@ let KNS =  {
 				return;
 			}
 			this.displays.push(display)
-			display.data = this;
 		}
 		/**
 		 * Deregister a display to kerbal

kerbalSearcher.js
@@ -128,6 +128,7 @@ Elements.elements.KerbalSearcher = class extends Elements.elements.dragged {
 		let kerbalDisplay = new KerbalChoiceDisplay();
 		this.vrtKbl = virtualKerbal;
 		this.kblDsp = kerbalDisplay;
+		kerbalDisplay.data = virtualKerbal;
 		virtualKerbal.addDisplay(kerbalDisplay);
 		let destinationSearch = (e) => {
 			this.destination_search_trigger();

commit 407e30990e28abccd3e334f74c47489eb6a8b7ec

Fix for name not shown, regression introduced in 051d447

kerbalMaker.js
@@ -132,8 +132,8 @@ Elements.elements.KerbalMaker = class extends Elements.elements.dragged {
 			self.hideWindow();
 		});
 		this.elements = elements;
-		this.newKerbal();
 		shadow.appendChild(template);
+		this.newKerbal();
 	}
 	/**
 	 * Reset the maker