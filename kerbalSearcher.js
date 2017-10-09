0b6bc91509f1106e585d0d6c12a671c30dc1e324
William Chow <431741580derp@gmail.com>
2017-10-10 09:52:40 +1100

    Implemenation of edit in kerbal-searcher

--- kerbalSearcher.js
+++ kerbalSearcher.js
@@ -253,7 +253,52 @@ Elements.elements.KerbalSearcher = class extends Elements.elements.dragged {
 	 * @return {Set}                  Ordered set of results
 	 */
 	static edit (search, nameList, threshold) {
-		return [];
+		let result = new Set();
+		for (let name in nameList) {
+			let distance = this.editDistance(name, search);
+			if (distance < threshold) {
+				result.add(name);
+			}
+		}
+		return result;
+	}
+	/**
+	 * Find the edit distance between two strings
+	 * @param  {String} string1 First string to compare
+	 * @param  {String} string2 Second string to compare
+	 * @return {Number}         The edit distance between the two strings
+	 * @private
+	 */
+	static editDistance (string1, string2) {
+		const m = string1.length + 1;
+		const n = string2.length + 1;
+		const fill = (length) => {
+			let result = [];
+			for (let i = 0; i < length; i++) {
+				result.push(0);
+			}
+			return result;
+		}
+		let d = [];
+		for (let i = 0; i < n; i++) {
+			d.push(fill(m));
+		}
+		for (let i = 0; i < m; i++) {
+			d[0][i] = i;
+		}
+		for (let i = 0; i < n; i++) {
+			d[i][0] = i;
+		}
+		for (let y = 1; y < n; y++) {
+			for (let x = 1; x < m; x++) {
+				if (string1[x-1] === string2[y-1]) {
+					d[y][x] = d[y-1][x-1];
+				} else {
+					d[y][x] = Math.min(d[y-1][x] + 1, d[y][x-1] + 1, d[y-1][x-1] + 1);
+				}
+			}
+		}
+		return d[n-1][m-1];
 	}
 	/**
 	 * Match names to the search, matching by prefix
