18804117be2dd437e99600f3d9b7111f71ffe032
William Chow <431741580derp@gmail.com>
2017-10-19 10:50:52 +1100

    kerbal-importer now uses kerbal-importer-import and kerbal-importer-export

--- kerbalImporter.js
+++ kerbalImporter.js
@@ -1,9 +1,9 @@
 'use strict';
-Elements.get('tabs', 'drag-element', 'kerbal-link');
+Elements.get('drag-element', 'kerbal-importer-import', 'kerbal-importer-export');
 
 {
 const main = async () => {
-await Elements.get('drag-element', 'kerbal-link');
+await Elements.get('drag-element');
 
 /**
  * Element that has a import/export window
@@ -16,172 +16,38 @@ Elements.elements.KerbalImporter = class extends Elements.elements.dragged {
 		super();
 		const self = this;
 		this.name = 'KerbalImporter';
-		this.__active_tab = 'Import';
-		this.database = this.database || 'default';
+		this.__database = this.database || 'default';
 		const shadow = this.attachShadow({mode: 'open'});
 		let template = Elements.importTemplate(this.name);
-
-
-		let tabs = [
-			template.querySelector('#import'),
-			template.querySelector('#export'),
-		];
-		let tabChange = (e) => {
-			requestAnimationFrame(() => {
-				for (let tab of tabs) {
-					tab.style.display = 'none';
-				}
-				let active;
-				switch (e.detail) {
-					case 'Import':
-						active = tabs[0];
-						self.fillImport();
-						break;
-					case 'Export':
-						active = tabs[1];
-						self.fillExport();
-						break;
-					default:
-						active = null;
-				}
-				if (active !== null) {
-					active.style.display = 'block'
-				} else {
-					self.__active_tab = e.detail;
-				}
-			})
-		};
-		template.querySelector('elements-tabs').addEventListener('change', tabChange);
-
-		let close = (e) => {
-			self.hideWindow();
-		}
-		template.querySelector('#Close').addEventListener('click', close);
-		template.querySelector('#exportDismiss').addEventListener('click', close);
-
-		let copy = template.querySelector('#exportCopy');
-		let exportArea = template.querySelector('#exportArea');
-		copy.addEventListener('click', (e) => {
-			e.preventDefault();
-			exportArea.select();
-			document.execCommand('copy');
+		let importer = template.querySelector('elements-kerbal-importer-import');
+		let exporter = template.querySelector('elements-kerbal-importer-export');
+		let tabWindow = template.querySelector('elements-tab-window');
+		tabWindow.parent = this;
+		Object.defineProperty(this, 'database', {
+			enumerable: true,
+			configurable: false,
+			get: () => {
+				return self.__database;
+			},
+			set: (value) => {
+				self.__database = value;
+				// Pass the change onto members
+				importer.database = value;
+				exporter.database = value;
+			},
 		});
-
-		let importArea = template.querySelector('#importArea');
-		let importFile = template.querySelector('#importFile');
-		let importCancel = template.querySelector('#importDismiss');
-		let importAccept = template.querySelector('#accept');
-		let importRaf = Elements.rafContext();
-		importFile.addEventListener('change', (e) => {
-			if (e.target.files.length < 1) {
-				return;
-			}
-			let file = e.target.files[0];
-			let reader = new FileReader(file);
-			reader.onload = (e) => {
-				let json = e.target.result;
-				importRaf((e) => {
-					importArea.innerHTML = json;
-				});
-			}
-			reader.readAsText(file);
-		});
-		importCancel.addEventListener('click', (e) => {
-			importRaf((e) => {
-				importArea.innerHTML = '';
-			});
-			close(e);
-		});
-		importAccept.addEventListener('click', (e) => {
-			self.consumeImport();
-			self.hideWindow();
-		})
 
 		shadow.appendChild(template);
 	}
-
-	/**
-	 * Unhide this element
-	 */
 	showWindow () {
 		super.showWindow();
-		if (this.__active_tab === 'Export') {
-			this.fillExport();
-		} else if (this.__active_tab === 'Import') {
-			this.fillImport();
-		}
+		let tabWindow = this.shadowRoot.querySelector('elements-tab-window');
+		tabWindow.showWindowInform();
 	}
-	/**
-	 * Populate the export field
-	 */
-	fillExport () {
-		let kdb = KerbalLink.get(this.database);
-		let json = JSON.stringify(kdb);
-		let exporter = this.shadowRoot.querySelector('#exportArea');
-		let blob = new Blob([json], {type: 'text/plain'});
-		let uri = URL.createObjectURL(blob);
-		let link = this.shadowRoot.querySelector('#exportDownloadLink');
-		link.href = uri;
-		requestAnimationFrame((e) => {
-			exporter.innerHTML = json;
-			exporter.select();
-		});
-	}
-	/**
-	 * Reset the import tab
-	 */
-	fillImport () {
-
-	}
-	/**
-	 * Do a import!
-	 */
-	consumeImport () {
-		let json = this.shadowRoot.querySelector('#importArea').value;
-		let kdb;
-		try {
-			kdb = KDB.fromJSON(json);
-		} catch (e) {
-			if (e instanceof SyntaxError) {
-				this.showWarning('Invalid input - not JSON');
-			} else if (e instanceof KNS.KDBParseError) {
-				this.showWarning('Invalid input - not a database');
-			} else {
-				this.showWarning('Unknown Error');
-			}
-			return;
-		}
-		this.showWarning(null);
-		let name = 'db' + KerbalLink.counter.toString();
-		let oldDB = this.database;
-		KerbalLink.set(name, kdb);
-		let UIs = KerbalLink.getUIAll(this.database);
-		for (let UIName of UIs) {
-			let UI = KerbalLink.getUI(oldDB, UIName);
-			UI.database = name;
-			KerbalLink.removeUI(this.database, UIName);
-			KerbalLink.registerUI(name, UIName, UI);
-			UI.database = name;
-		}
-		KerbalLink.setNextLoad(name);
-		KerbalLink.save(name);
-		KerbalLink.delete(oldDB);
-	}
-	/**
-	 * Show a error message for invalid imports
-	 * @param  {?String} [errorMessage=null] Error message to show. null to hide the warning
-	 */
-	showWarning (errorMessage = null) {
-		let warning = this.shadowRoot.querySelector('#invalidImport');
-		let warningText = warning.querySelector('#invalidImportText');
-		requestAnimationFrame((e) => {
-			if (errorMessage === null) {
-				warning.style.display = 'none';
-			} else {
-				warning.style.display = 'flex';
-				warningText.innerHTML = errorMessage;
-			}
-		});
+	hideWindow () {
+		super.hideWindow();
+		let tabWindow = this.shadowRoot.querySelector('elements-tab-window');
+		tabWindow.hideWindowInform();
 	}
 }
 
