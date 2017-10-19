fb66b9094c8c8710d94bb37fac497c39f0b88035
William Chow <431741580derp@gmail.com>
2017-10-19 14:18:10 +1100

    Inital work spliting kerbal-searcher

---
+++ kerbalSearcherDestination.js
@@ -0,0 +1,235 @@
+'use strict';
+
+Elements.get('kerbal', 'KDB', 'tab-window', 'kerbal-link');
+{
+const main = async () => {
+
+await Elements.get('tab-window', 'KDB');
+
+/**
+ * Like kerbalJobDisplay, but built for kerbal-search
+ * @type {Object}
+ * @augments KerbalJobDisplay
+ * @implements KerbalDisplay
+ */
+let KerbalChoiceDisplay = class extends KerbalJobDisplay {
+	/**
+	 * Make a display element
+	 * @param  {String} place Location
+	 * @param  {Number} value Depth of visited required
+	 * @return {HTMLElement} Element representing place+value
+	 */
+	makeJobElement (place, value) {
+		let div = document.createElement('div');
+		div.classList.add('results');
+		let p = super.makeJobElement(place, value);
+		p.classList.add('job');
+		div.appendChild(p);
+		let button = document.createElement('button');
+		button.value = place;
+		button.innerHTML = 'Remove';
+		button.classList.add('results');
+		button.addEventListener('click', (e) => {
+			let event = new CustomEvent('remove', {detail: place, value: place,});
+			this.display.dispatchEvent(event);
+		});
+		div.appendChild(button);
+		return div;
+	}
+	/**
+	 * Update a job display element
+	 * @param  {HTMLElement} element Job display element
+	 * @param  {String} place   Destination of mission
+	 * @param  {int} value   Depth of mission
+	 */
+	changeJobElement (element, place, value) {
+		element.querySelector('p').innerHTML = place + ' ' + KNS.valueToJob(value);
+	}
+};
+
+
+/**
+ * UI to search through kerbals by destination
+ * @type {Object}
+ * @augments Elements.elements.tabbed
+ * @property {String} database Name of the database to look up
+ */
+Elements.elements.KerbalSearcherDestination = class extends Elements.elements.backbone {
+	constructor () {
+		super();
+		const self = this;
+
+		this.name = 'KerbalSearcherDestination';
+		/**
+		 * Which database to search
+		 * @type {String}
+		 * @private
+		 */
+		this.__database = this.database || 'default';
+		Object.defineProperty(this, 'database', {
+			enumerable: true,
+			configurable: false,
+			get: () => {
+				return self.__get_database();
+			},
+			set: (value) => {
+				self.__set_database(value);
+			},
+		});
+		const shadow = this.attachShadow({mode: 'open'});
+		let template = Elements.importTemplate(this.name);
+		this.__displayMap = new Map();
+
+		/**
+		 * KNS.Kerbal already has job tracker
+		 * @type {KNS.Kerbal}
+		 */
+		let virtualKerbal = new KNS.Kerbal();
+		let kerbalDisplay = new KerbalChoiceDisplay();
+		this.vrtKbl = virtualKerbal;
+		this.kblDsp = kerbalDisplay;
+		kerbalDisplay.data = virtualKerbal;
+		virtualKerbal.addDisplay(kerbalDisplay);
+		let destinationSearch = (e) => {
+			this.destination_search_trigger();
+		};
+		let addDestination = (e) => {
+			let locationUI = self.shadowRoot.querySelector('#AnsAddPlace');
+			let depthUI = self.shadowRoot.querySelector('#AnsAddValue');
+			let location = locationUI.value;
+			let depth = depthUI.value;
+			virtualKerbal.removeJob(location, KNS.MAX_JOB_VALUE);
+			virtualKerbal.addJob(location, parseInt(depth));
+			locationUI.focus();
+			destinationSearch();
+		};
+		let removeDestination = (e) => {
+			let location = e.detail;
+			virtualKerbal.removeJob(location, KNS.MAX_JOB_VALUE);
+			destinationSearch();
+		};
+		template.querySelector('#lower').addEventListener('change', destinationSearch);
+		template.querySelector('#tourism').addEventListener('change', destinationSearch);
+
+		template.querySelector('#destination-search').appendChild(kerbalDisplay.display);
+		template.querySelector('#AnsAddConfirm').addEventListener('click', addDestination);
+		kerbalDisplay.display.addEventListener('remove', removeDestination);
+		kerbalDisplay.display.id = 'jobs';
+
+		let results = template.querySelector('#results');
+		results.addEventListener('touchstart', (e) => {
+			// Check for overflow
+			if (results.clientHeight !== results.scrollHeight) {
+				this.parentElement.touch_reset();
+				e.stopPropagation();
+				e.stopImmediatePropagation();
+			}
+
+		});
+
+		shadow.appendChild(template);
+	}
+	/**
+	 * Getter for database
+	 * @private
+	 * @return {String} Name of database
+	 */
+	__get_database () {
+		return this.__database;
+	}
+	/**
+	 * Setter for database
+	 * @param {String} value Name of database to search
+	 * @private
+	 */
+	__set_database (value) {
+		this.emptyNodes();
+		this.__database = value;
+		this.destination_search_trigger(true);
+	}
+	/**
+	 * Search kerbals by destination
+	 * @param  {Object} jobList place -> value mapping
+	 * @param  {Boolean} lower   Whether to include missions shallower than the search
+	 * @param  {Boolean} tourism Whether to only include tourists
+	 * @param  {String[]} [excludes] Name of kerbals to exclude from the search
+	 * @return {KNS.Kerbal[]}    An ordered array of kerbals matching
+	 */
+	destination_search (jobList, lower, tourism, excludes = []) {
+		let locations = [];
+		let lower_results = [];
+		let results = [];
+		let reduce = KNS.reducePlaceList(jobList);
+
+		for (let location in reduce) {
+			locations.push({
+				place: location,
+				value: jobList[location],
+			});
+		}
+		let check = (location, kerbal) => {
+			const value = kerbal.jobs[location.place];
+			if (value === location.value) {
+				return true;
+			}
+			return false;
+		};
+		let lower_check = (location, kerbal) => {
+			const value = kerbal.jobs[location.place];
+			if (lower && value < location.value && value !== 0) {
+				return true;
+			}
+			return false;
+		};
+		let kdb = KerbalLink.get(this.database);
+		for (let kerbalName of kdb.kerbals) {
+			if (excludes.includes(kerbalName)) {
+				continue;
+			}
+			let kerbal = kdb.getKerbal(kerbalName);
+			if (tourism && kerbal.text !== 'Tourist') {
+				continue;
+			}
+			let flag = true;
+			let lower_flag = true;
+			for (let location of locations) {
+				if (!(check(location, kerbal))) {
+					flag = false;
+				}
+				if (!(lower_check(location, kerbal))) {
+					lower_flag = false;
+					continue;
+				}
+			}
+			if (flag === true) {
+				results.push(kerbal);
+			} else if (lower_flag === true) {
+				lower_results.push(kerbal);
+			}
+		}
+		return results.concat(lower_results);
+	}
+	/**
+	 * Start a destination search from information in the UI
+	 * @param  {Boolean} [override=false] Flag to force a refresh, even if the current search is already displayed
+	 * @param  {String[]} [excludes=[]] List of names to exclude from the search
+	 */
+	destination_search_trigger (override = false, excludes = []) {
+		let virtualKerbal = this.vrtKbl;
+		if (virtualKerbal.size === 0) {
+			this.display_results([]);
+		} else {
+			let lower = this.shadowRoot.querySelector('#lower').checked;
+			let tourist = this.shadowRoot.querySelector('#tourism').checked;
+			let results = this.destination_search(virtualKerbal.jobs, lower, tourist, excludes);
+			this.display_results(results);
+		}
+		this.__lastSearch = 'destination';
+	}
+}
+
+Elements.load('kerbalSearcherDestinationTemplate.html', Elements.elements.KerbalSearcherDestination, 'elements-kerbal-searcher-destination');
+};
+
+main();
+}
---
+++ kerbalSearcherKerbal.js
@@ -0,0 +1,262 @@
+'use strict';
+
+Elements.get('kerbal', 'KDB', 'drag-down', 'tab-window', 'kerbal-link');
+{
+const main = async () => {
+
+await Elements.get('tab-window');
+
+
+/**
+ * UI to search through kerbals by name
+ * @type {Object}
+ * @augments Elements.elements.tabbed
+ * @property {String} database Name of the database to look up
+ */
+Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed {
+	constructor () {
+		super();
+		const self = this;
+
+		this.name = 'KerbalSearcherKerbal';
+		/**
+		 * Which database to search
+		 * @type {String}
+		 * @private
+		 */
+		this.__database = this.database || 'default';
+		Object.defineProperty(this, 'database', {
+			enumerable: true,
+			configurable: false,
+			get: () => {
+				return self.__get_database();
+			},
+			set: (value) => {
+				self.__set_database(value);
+			},
+		});
+		const shadow = this.attachShadow({mode: 'open'});
+		let template = Elements.importTemplate(this.name);
+		this.__lastValue = '';
+		this.__displayMap = new Map();
+
+		let searcher = template.querySelector('#nameInput');
+		searcher.addEventListener('keyup', (e) => {
+			self.kerbal_search_trigger();
+		});
+
+		let updater = (e) => {
+			self.kerbal_search_trigger(true);
+		}
+		let checkboxes = template.querySelector('#checkboxes');
+		for (let checkbox of checkboxes.querySelectorAll('input')) {
+			checkbox.addEventListener('change', updater);
+		}
+
+		let results = template.querySelector('#results');
+		results.addEventListener('touchstart', (e) => {
+			// Check for overflow
+			if (results.clientHeight !== results.scrollHeight) {
+				this.parentElement.touch_reset();
+				e.stopPropagation();
+				e.stopImmediatePropagation();
+			}
+
+		});
+
+		shadow.appendChild(template);
+	}
+	/**
+	 * Getter for database
+	 * @private
+	 * @return {String} Name of database
+	 */
+	__get_database () {
+		return this.__database;
+	}
+	/**
+	 * Setter for database
+	 * @param {String} value Name of database to search
+	 * @private
+	 */
+	__set_database (value) {
+		this.emptyNodes();
+		this.__database = value;
+		this.kerbal_search_trigger(true);
+	}
+	/**
+	 * Match names to the search, matching by prefix
+	 * @param  {String} search        Name to search for
+	 * @param  {String[]} nameList    List of names to search through
+	 * @return {Set}                  Ordered set of results
+	 */
+	static prefix (search, nameList) {
+		search = search.toLowerCase();
+		let checker = (name) => {
+			name = name.toLowerCase();
+			if (name.indexOf(search) === 0) {
+				return true;
+			}
+			return false;
+		}
+		let results = new Set();
+		for (let name of nameList) {
+			if (checker(name)) {
+				results.add(name);
+			}
+		}
+		return results;
+	}
+	/**
+	 * Match names to the search, matching by fuzzy search
+	 * @param  {String} search        Name to search for
+	 * @param  {String[]} nameList    List of names to search through
+	 * @return {Set}                  Ordered set of results
+	 */
+	static fuzzy (search, nameList) {
+		search = search.toLowerCase();
+		let checker = (name) => {
+			name = name.toLowerCase();
+			let position = 0;
+			for (let char of name) {
+				if (char === search[position]) {
+					position += 1;
+				}
+			}
+			if (position === search.length) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		let results = new Set();
+		for (let name of nameList) {
+			if (checker(name)) {
+				results.add(name);
+			}
+		}
+		return results;
+	}
+	/**
+	 * Match names to the search, matching by prefix
+	 * @param  {String} search        Name to search for
+	 * @param  {String[]} nameList    List of names to search through
+	 * @param  {Number} threshold     Edit distance limit
+	 * @return {Set}                  Ordered set of results
+	 */
+	static edit (search, nameList, threshold) {
+		let result = new Set();
+		for (let name of nameList) {
+			let distance = this.editDistance(name, search);
+			if (distance < threshold) {
+				result.add(name);
+			}
+		}
+		return result;
+	}
+	/**
+	 * Find the edit distance between two strings
+	 * @param  {String} string1 First string to compare
+	 * @param  {String} string2 Second string to compare
+	 * @return {Number}         The edit distance between the two strings
+	 * @private
+	 */
+	static editDistance (string1, string2) {
+		const m = string1.length + 1;
+		const n = string2.length + 1;
+		const fill = (length) => {
+			let result = [];
+			for (let i = 0; i < length; i++) {
+				result.push(0);
+			}
+			return result;
+		}
+		let d = [];
+		for (let i = 0; i < n; i++) {
+			d.push(fill(m));
+		}
+		for (let i = 0; i < m; i++) {
+			d[0][i] = i;
+		}
+		for (let i = 0; i < n; i++) {
+			d[i][0] = i;
+		}
+		for (let y = 1; y < n; y++) {
+			for (let x = 1; x < m; x++) {
+				if (string1[x-1] === string2[y-1]) {
+					d[y][x] = d[y-1][x-1];
+				} else {
+					d[y][x] = Math.min(d[y-1][x] + 1, d[y][x-1] + 1, d[y-1][x-1] + 1);
+				}
+			}
+		}
+		return d[n-1][m-1];
+	}
+	/**
+	 * Match names to the search, matching by prefix
+	 * @param  {String} search        Name to search for
+	 * @param  {String[]} nameList    List of names to search through
+	 * @return {Set}                  Ordered set of results
+	 */
+	static exact (search, nameList) {
+		if (nameList.includes(search)) {
+			return new Set([search]);
+		} else {
+			return [];
+		}
+	}
+	/**
+	 * Search for a kerbal, deriving options from the UI
+	 * @param  {String} search        Name to search for
+	 * @param  {Array}  [excludes=[]] List of names to exclude
+	 * @return {Set}               Set of matches, in order of likeness
+	 */
+	search (search, excludes = []) {
+		let nameList = Array(...KerbalLink.get(this.database).kerbals);
+		for (let name of excludes) {
+			let index = nameList.indexOf(name);
+			if (index !== -1) {
+				nameList.splice(index, 1);
+			}
+		}
+		let prefix = this.shadowRoot.querySelector('#prefix').checked;
+		let fuzzy = this.shadowRoot.querySelector('#fuzzy').checked;
+		let edit = this.shadowRoot.querySelector('#edit').checked;
+		let result = new Set(this.constructor.exact(search, nameList));
+		if (search === '') {
+			return result;
+		}
+		if (prefix) {
+			result = new Set([...result, ...this.constructor.prefix(search, nameList)]);
+		}
+		if (fuzzy) {
+			result = new Set([...result, ...this.constructor.fuzzy(search, nameList)]);
+		}
+		if (edit) {
+			const threshold = 2;
+			result = new Set([...result, ...this.constructor.edit(search, nameList, threshold)]);
+		}
+		return result;
+	}
+	/**
+	 * Start a kerbal search from information in the UI
+	 * @param  {Boolean} [override=false] Flag to force a refresh, even if the current search is already displayed
+	 * @param  {String[]} [excludes=[]] List of names to exclude from the search
+	 */
+	kerbal_search_trigger (override = false, excludes = []) {
+		let searcher = this.shadowRoot.querySelector('#nameInput');
+		let search = searcher.value
+		search = Elements.nameSanitizer(search);
+		if (search !== this.__lastValue || override) {
+			this.display_results(this.__resolve_names(this.search(search, excludes)));
+			this.__lastValue = search;
+		}
+		this.__lastSearch = 'kerbal';
+	}
+}
+
+Elements.load('kerbalSearcherKerbalTemplate.html', Elements.elements.KerbalSearcherKerbal, 'elements-kerbal-searcher-kerbal');
+};
+
+main();
+}
