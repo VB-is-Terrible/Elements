31ff7861c09561e6e5a4389e0797decddeacfcaf
William Chow <431741580derp@gmail.com>
2017-10-23 10:42:33 +1100

    More work on kerbal-maker-group

--- kerbalMakerGroup.js
+++ kerbalMakerGroup.js
@@ -4,13 +4,13 @@ Elements.get('kerbal-searcher-kerbal');
 {
 const main = async () => {
 
-await Elements.get('tab-window', 'KDB');
+await Elements.get('tab-window', 'KDB', 'main');
 
 /**
  * UI to make a KNS.Group
  * @type {Object}
  * @property {Object} UI Store of useful UI elements
- * @property {KNS.Kerbal} group Group been made
+ * @property {KNS.Group} group Group been made
  * @property {String} database Name of the database to look up
  * @augments Elements.elements.tabbed
  */
@@ -21,6 +21,8 @@ Elements.elements.KerbalMakerGroup = class extends Elements.elements.tabbed {
 
 		this.name = 'KerbalMakerGroup';
 		this.__database = this.database || 'default';
+		this.group = new KNS.Group();
+		this.nameValid = false;
 		const shadow = this.attachShadow({mode: 'open'});
 		let template = Elements.importTemplate(this.name);
 		let searcher = template.querySelector('elements-kerbal-searcher-kerbal');
@@ -36,12 +38,35 @@ Elements.elements.KerbalMakerGroup = class extends Elements.elements.tabbed {
 				searcher.database = value;
 			},
 		});
+
+		let includeCallback = (name) => {
+			let kerbal = KerbalLink.get(this.database).getKerbal(name);
+			this.group.addKerbal(kerbal);
+		}
+		let ansName = template.querySelector('#AnsName');
+		ansName.addEventListener('change', (e) => {
+			let name = ansName.value;
+			if (name === '') {
+				//TODO: Show warning
+				this.nameValid = false;
+			} else {
+				this.nameValid = true;
+				// TODO: Hide warning
+			}
+			this.group.name = name;
+		});
+		let ansText = template.querySelector('#AnsText');
+		ansText.addEventListener('change', (e) => {
+			let text = ansText.value;
+			this.group.text = text;
+		});
+
 		let canceler = (e) => {
 			e.stopPropagation();
 			e.stopImmediatePropagation();
 			// e.preventDefault();
-			this.parentElement.drag_reset();
-			this.parentElement.touch_reset();
+			self.drag_reset();
+			self.touch_reset();
 		};
 		for (let input of template.querySelectorAll('input')) {
 			input.addEventListener('mousedown', canceler);
