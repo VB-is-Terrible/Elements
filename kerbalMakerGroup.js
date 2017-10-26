ebc9ca91dfaa55ce913bbe6fa851473dd9d0548c
William Chow <431741580derp@gmail.com>
2017-10-26 23:23:53 +1100

    Fix dependancies

--- kerbalMakerGroup.js
+++ kerbalMakerGroup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Elements.get('kerbal-searcher-kerbal', 'kerbal-display-text');
+Elements.get('kerbal-searcher-kerbal', 'kerbal-display-text', 'tab-window', 'KDB', 'main');
 {
 const main = async () => {
 
