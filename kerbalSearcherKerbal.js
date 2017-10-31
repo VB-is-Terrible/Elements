8afde0fc47c09dab12933937c009fe90d3bc090d
William Chow <431741580derp@gmail.com>
2017-10-31 12:45:49 +1100

    kerbal-searcher-kerbal now inherits from kerbal-searcher-common

--- kerbalSearcherKerbal.js
+++ kerbalSearcherKerbal.js
@@ -1,73 +1,33 @@
 'use strict';
 
-Elements.get('kerbal', 'KDB', 'drag-down', 'tab-window', 'kerbal-link');
+Elements.get('KDB', 'drag-down', 'tab-window', 'kerbal-link', 'kerbal-searcher-common');
 {
 const main = async () => {
 
-await Elements.get('tab-window', 'KDB');
-
-/**
- * A KerbalDisplay used to listen to delete and rename callbacks
- * @type {Object}
- * @augments BlankKDBDisplay
- * @implements KDBDisplay
- */
-let KDBListener = class extends BlankKDBDisplay {
-	/**
-	 * Build a listener
-	 * @param  {KDB} database database to listen
-	 * @param  {Elements.elements.KerbalSearcher} searcher Searcher to inform
-	 */
-	constructor (database, searcher) {
-		super();
-		this.searcher = searcher;
-		this.database = database;
-		database.addDisplay(this);
-	}
-	deleteKerbal (kerbal) {
-		this.searcher.delete_inform(kerbal);
-	}
-	renameKerbal (oldName, newName) {
-		this.searcher.rename_inform(oldName, newName);
-	}
-	delete () {
-		this.database.removeDisplay(this);
-	}
-}
+await Elements.get('tab-window', 'KDB', 'kerbal-searcher-common');
 
 /**
  * UI to search through kerbals by name
  * @type {Object}
- * @augments Elements.elements.tabbed
+ * @augments Elements.elements.KerbalSearcherCommon
  * @property {String} database Name of the database to look up
  * @property {String} action   Text to display in buttons next to results
  * @property {Function} actionCallback Function to call with the name of kerbal whose action was clicked
  */
-Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed {
+Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.KerbalSearcherCommon {
 	constructor () {
 		super();
 		const self = this;
 
 		this.name = 'KerbalSearcherKerbal';
-		this.resultsRAF = Elements.rafContext();
 		/**
 		 * Which database to search
 		 * @type {String}
 		 * @private
 		 */
-		this.__database = this.database || 'default';
-		Object.defineProperty(this, 'database', {
-			enumerable: true,
-			configurable: false,
-			get: () => {
-				return self.__get_database();
-			},
-			set: (value) => {
-				self.__set_database(value);
-			},
-		});
-		this.action = this.action || 'Edit';
-		this.actionCallback = this.actionCallback || ((name) => {
+		this.__database = 'default';
+		this.action = 'Edit';
+		this.actionCallback = ((name) => {
 			let editor = KerbalLink.getUI(self.database, 'editor');
 			if (editor) {
 				let kerbal = KerbalLink.get(self.database).getKerbal(name);
@@ -79,9 +39,6 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 		const shadow = this.attachShadow({mode: 'open'});
 		let template = Elements.importTemplate(this.name);
 		this.__lastValue = '';
-		this.__virtualDisplayMap = new Map();
-		this.__listener = null;
-		this.__results_length = 0;
 
 		let searcher = template.querySelector('#nameInput');
 		searcher.addEventListener('keyup', (e) => {
@@ -114,13 +71,14 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 			}
 		}
 		shadow.appendChild(template);
+		this.applyPriorProperties('database', 'action', 'actionCallback');
 	}
 	/**
 	 * Getter for database
 	 * @private
 	 * @return {String} Name of database
 	 */
-	__get_database () {
+	get database () {
 		return this.__database;
 	}
 	/**
@@ -128,7 +86,7 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 	 * @param {String} value Name of database to search
 	 * @private
 	 */
-	__set_database (value) {
+	set database (value) {
 		this.emptyNodes();
 		this.__database = value;
 		this.kerbal_search_trigger(true);
@@ -236,8 +194,8 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 					d[y][x] = d[y-1][x-1];
 				} else {
 					d[y][x] = Math.min(d[y-1][x] + 1, d[y][x-1] + 1, d[y-1][x-1] + 1);
-				}
 			}
+		}
 		}
 		return d[n-1][m-1];
 	}
@@ -302,94 +260,6 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 		}
 	}
 	/**
-	 * Show array on screen, establishes delete/rename watcher
-	 * @param  {KNS.Kerbal[]} results Array of results, best match to worst
-	 */
-	display_results (results) {
-		let itemHolder = this.shadowRoot.querySelector('#results');
-		let name = this.shadowRoot.querySelector('#resultsTitle');
-		let string = this.constructor.resultsString(results.length);
-		this.emptyNodes();
-
-		let queue = [];
-		for (let kerbal of results) {
-			let display = this.__makeDisplay(kerbal);
-			this.__virtualDisplayMap.set(kerbal, display);
-			queue.push(display);
-		}
-		this.__results_length = results.length;
-
-		let database = KerbalLink.get(this.database);
-		this.__listener = new KDBListener(database, this);
-
-		this.resultsRAF((e) => {
-
-			for (let display of queue) {
-				itemHolder.appendChild(display);
-			}
-			name.innerHTML = string;
-			if (results.length === 0) {
-				itemHolder.style.display = 'none';
-			} else {
-				itemHolder.style.display = 'block';
-			}
-			this.update = null;
-		});
-	}
-	/**
-	 * Resets the results display
-	 */
-	emptyNodes () {
-		// TODO: raf' this function
-		let kdb = KerbalLink.get(this.database);
-		let holder = this.shadowRoot.querySelector('#results');
-		for (var i = holder.children.length - 1; i >= 0; i--) {
-			let kerbal = holder.children[i].children[0];
-			if (kerbal.data !== null) {
-				kerbal.data = null;
-				this.__virtualDisplayMap.delete(kerbal.data);
-			} else {
-				console.warn('Could not find a kerbal in its holder');
-			}
-			holder.removeChild(holder.children[i]);
-		}
-		if (this.__virtualDisplayMap.size > 0) {
-			this.__virtualDisplayMap = new Map();
-		}
-	}
-	/**
-	 * Causes the selected kerbal to be sent to the editor
-	 * @private
-	 * @param  {KNS.Kerbal} kerbal Kerbal been clicked
-	 */
-	editor (kerbal) {
-		let name = kerbal.name;
-		this.actionCallback(name);
-	}
-	/**
-	 * Make a new div displaying a search result
-	 * @param  {KNS.Kerbal} kerbal Kerbal to show
-	 * @return {HTMLElement}       A div containing the kerbal, edit button
-	 * @private
-	 */
-	__makeDisplay (kerbal) {
-		let div = document.createElement('div');
-		div.classList.add('results');
-		let display = document.createElement('elements-kerbal');
-		display.data = kerbal;
-		display.menuvisible = false;
-		display.deleter = false;
-		div.appendChild(display);
-		let button = document.createElement('button');
-		button.innerHTML = this.action;
-		button.classList.add('results');
-		button.addEventListener('click', (e) => {
-			this.editor(kerbal);
-		});
-		div.appendChild(button);
-		return div;
-	}
-	/**
 	 * Resovle an array of kerbal names into kerbals
 	 * @param  {String[]} array List of names
 	 * @return {KNS.Kerbal[]}   List of kerbals with those names
@@ -402,44 +272,6 @@ Elements.elements.KerbalSearcherKerbal = class extends Elements.elements.tabbed 
 			result.push(kdb.getKerbal(name));
 		}
 		return result;
-	}
-	/**
-	 * Inform the searcher of a kerbal deletion
-	 * @param  {KNS.Kerbal} kerbal Kerbal been deleted
-	 */
-	delete_inform (kerbal) {
-		if (!this.__virtualDisplayMap.has(kerbal)) {return;}
-		let results = this.shadowRoot.querySelector('#resultsTitle');
-		let display = this.__virtualDisplayMap.get(kerbal);
-		this.__results_length -= 1;
-		let string = this.constructor.resultsString(this.__results_length);
-		requestAnimationFrame((e) => {
-		    display.remove();
-			results.innerHTML = string;
-		});
-	}
-	/**
-	 * Inform the searcher of a kerbal rename
-	 * @param  {String} oldName Previous name of kerbal
-	 * @param  {String} newName New name of kerbal
-	 */
-	rename_inform (oldName, newName) {}
-	/**
-	 * Generates the string to display in #Results
-	 * @param  {Number} amount Number of results
-	 * @return {String}        String if "Results: n matches"
-	 */
-	static resultsString (amount) {
-		let string = 'Results';
-		if (amount !== 0) {
-			string += ': ';
-			string += amount.toString();
-			string += ' match';
-			if (amount > 1) {
-				string += 'es';
-			}
-		}
-		return string;
 	}
 }
 
