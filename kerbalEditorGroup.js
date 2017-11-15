57314ebcd2c93bc77bbaed0e4109cdf4e25f34f4
William Chow <431741580derp@gmail.com>
2017-11-16 06:56:43 +1100

    Start making kerbal-editor-group

--- kerbalEditorGroup.js
+++ kerbalEditorGroup.js
@@ -4,18 +4,40 @@ Elements.get('kerbal-group-tag', 'KDB', 'drag-down');
 {
 const main = async () => {
 
-await Elements.get();
-Elements.elements.KerbalEditorGroup = class extends Elements.elements.backbone {
+await Elements.get('KDB');
+
+/**
+ * UI to edit a KNS.Group
+ * @type {Object}
+ * @property {KNS.Group} group Group been edit - note: this is a copy, not the original
+ * @property {String} database Name of the database to look up
+ * @augments Elements.elements.backbone2
+ * @augments Elements.elements.tabbed2
+ */
+Elements.elements.KerbalEditorGroup = class extends Elements.elements.backbone2 {
 	constructor () {
 		super();
 		const self = this;
 
 		this.name = 'KerbalEditorGroup';
+		this.__group = null;
+		this.__database = null;
 		const shadow = this.attachShadow({mode: 'open'});
 		let template = Elements.importTemplate(this.name);
-
-		//Fancy code goes here
 		shadow.appendChild(template);
+		this.applyPriorProperties('database', 'group');
+	}
+	get group () {
+		return this.__group;
+	}
+	set group (value) {
+		this.__group = value;
+	}
+	get database () {
+		return this.__database;
+	}
+	set database (value) {
+		this.__database = value;
 	}
 }
 
