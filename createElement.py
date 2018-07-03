aecd606f13b290340d35a4a5b9684b31f9a4ad1c
William Chow <431741580derp@gmail.com>
2018-07-03 19:25:29 +1000

    updated createElement

--- createElement.py
+++ createElement.py
@@ -1,22 +1,19 @@
 import os
-
-# $0 = uncapitalized name
+from parser import name_resolver
+# $0 = file path name
 # $1 = capitlized name
 # $2 = name with dashes
 
 def removeDashes(s):
-	l = [x.capitalize() for x in s.split('-')]
+	l = [x.capitalize() for x in s.split('/')]
 	n1 = ''.join(l)
-	l[0] = l[0][:1].lower() + l[0][1:]
-	n0 = ''.join(l)
-	return n0, n1
+	return n1
 
 def checkExists(name0):
-	files = os.listdir()
-	suffixes = ['.js', '.css', '.html', 'Template.html', '']
+	suffixes = ['/element.js', '/style.css', '/template.html']
 	l = [name0 + suffix for suffix in suffixes]
 	for name in l:
-		if name in files:
+		if os.path.isfile(name):
 			return True
 	else:
 		return False
@@ -32,25 +29,34 @@ def copy(fin, fout, n0, n1, n2):
 	fin.close()
 	fout.close()
 
-def main():
+
+def main2():
 	name2 = input('Enter element name (with dashes): ')
 	if not name2:
 		print('Empty input')
 		return
 	else:
-		name0, name1 = removeDashes(name2)
+		name0 = name_resolver(name2)
+		name1 = removeDashes(name0)
+	module_name = name0.split('/')[-1]
+	if module_name[0].isupper():
+		print('Can\'t create module')
+		return
+
 	if checkExists(name0):
 		print('File already exists')
 		return
+
 	print(name0, name1, name2)
+	os.makedirs(name0, exist_ok=True)
 	try:
-		copy('Templates/nameTemplate.html', name0 + 'Template.html', name0, name1, name2)
-		copy('Templates/template.css', name0 + '.css', name0, name1, name2)
-		copy('Templates/template.js', name0 + '.js', name0, name1, name2)
+		copy('Templates/nameTemplate.html', name0 + '/template.html', name0, name1, name2)
+		copy('Templates/template.css', name0 + '/style.css', name0, name1, name2)
+		copy('Templates/template.js', name0 + '/element.js', name0, name1, name2)
 	except Exception as e:
 		print('The following error occured during copying')
 		print(e)
-		l = [name0 + suffix for suffix in ['Template.html', '.css', '.js']]
+		l = [name0 + suffix for suffix in ['/template.html', '/style.css', '/element.js']]
 		for name in l:
 			try:
 				os.remove(name)
