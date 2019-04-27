e6493ba2ca986bd6571ff621827987737cc9fd88
William Chow <431741580derp@gmail.com>
2019-04-28 05:16:12 +1000

    Update facebook-post

--- elements/fakebook/post/element.js
+++ elements/fakebook/post/element.js
@@ -22,6 +22,32 @@ Elements.elements.FakebookPost = class FakebookPost extends Elements.elements.ba
 		//Fancy code goes here
 		shadow.appendChild(template);
 	}
+	setName (name) {
+		let body = this.shadowRoot.querySelector('#pseudoBody');
+		let name_out = body.querySelector('p.name');
+		name_out.innerHTML = Elements.nameSanitizer(name);
+	}
+	setTime (time) {
+		let body = this.shadowRoot.querySelector('#pseudoBody');
+		let time_out = body.querySelector('p.time');
+		let time_obj = new Date(time);
+		time_out.innerHTML = time_obj.toISOString();
+	}
+	setProfile (img_location) {
+		let body = this.shadowRoot.querySelector('#pseudoBody');
+		let profile = body.querySelector('#profile');
+		profile.src = img_location;
+	}
+	setText (text) {
+		let body = this.shadowRoot.querySelector('#pseudoBody');
+		let text_out = body.querySelector('.text');
+		text_out.innerHTML = Elements.nameSanitizer(text);
+	}
+	setPicture (img_location) {
+		let body = this.shadowRoot.querySelector('#pseudoBody');
+		let picture = body.querySelector('#picture');
+		picture.src = img_location;
+	}
 	connectedCallback () {
 		super.connectedCallback();
 	}
