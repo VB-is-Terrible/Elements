f3ac62ef75a5daf05087c6b57475f79723a27433
William Chow <431741580derp@gmail.com>
2019-09-27 17:58:36 +1000

    Got up to the progress bar

--- elements/projects/project/full/editor/editor.mjs
+++ elements/projects/project/full/editor/editor.mjs
@@ -21,9 +21,29 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 		 * @type {Projects.Project}
 		 */
 		this._data = null;
+		this._resetChanges();
 		// UI update handlers
-
+		template.querySelector('#cancel').addEventListener('click', this.cancel);
+		template.querySelector('#Title').addEventListener('input',
+			(e) => {this._changeTitle(e);});
+		template.querySelector('#AnsProgress').addEventListener('input',
+			(e) => {this._changeCounter(e);});
+		template.querySelector('#AnsProgressAmount').addEventListener('input',
+			(e) => {this._changeRequired(e, false);});
+		template.querySelector('#AnsProgressAmount').addEventListener('change',
+			(e) => {this._changeRequired(e, true);});
+		template.querySelector('#projectProgressRange').addEventListener('input',
+			(e) => {this._changeProgress(e);});
+		template.querySelector('#projectProgressDecrease').addEventListener('click',
+			(e) => {this._decrementProgress(e);});
+		template.querySelector('#projectProgressIncrease').addEventListener('click',
+			(e) => {this._incrementProgress(e);});
+		template.querySelector('#projectMeta').addEventListener('change',
+			(e) => {this._changeMeta(e);});
+		template.querySelector('#projectDesc').addEventListener('input',
+			(e) => {this._changeDescription(e);});
 		shadow.appendChild(template);
+		this.applyPriorProperty('data', null);
 	}
 	connectedCallback () {
 		super.connectedCallback();
@@ -48,6 +68,9 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 			return;
 		}
 		this._display(value);
+		this._changes.progress_amount = value.progress;
+		this._changes.required_amount = value.required;
+		this._changes.counter = value.counter;
 	}
 	/**
 	 * Reset the editor state
@@ -58,6 +81,14 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 		this._writeProgress(1, 1, true);
 		this._writeDependencies([]);
 		this._writeTitle('Blank Project');
+		this._resetChanges();
+	}
+	_resetChanges () {
+		this._changes = {
+			required_amount: 0,
+			progress_amount: 0,
+			counter: false,
+		};
 	}
 	/**
 	 * Show the project state for editing
@@ -66,7 +97,7 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 	_display (project) {
 		this._writeMeta(project.meta);
 		this._writeDesc(project.desc);
-		this._writeProgress(project.required, project.progress);
+		this._writeProgress(project.required, project.progress, project.counter);
 		this._writeDependencies(project.dependencies);
 		this._writeTitle(project.name);
 	}
@@ -99,7 +130,7 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 	 * @private
 	 */
 	_writeDependencies (dependencies) {
-		// TODO: Make an element that shows dependencies
+		// : Make an element that shows dependencies
 	}
 	_writeTitle (title) {
 		this._writeElement(title, '#pageTitle');
@@ -145,7 +176,61 @@ export class ProjectsProjectFullEditor extends Elements.elements.backbone3 {
 		progress_range.max = required;
 		progress_range.value = progress;
 	}
-
+	_changeTitle (e) {}
+	_changeCounter (e) {
+		const required_input = this.shadowQuery('#AnsProgressAmount');
+		if (e.path[0].checked) {
+			requestAnimationFrame((e) => {
+				required_input.disabled = false;
+			});
+			this._writeProgress(this._changes.required_amount,
+			                    this._changes.progress_amount,
+				            true);
+		} else {
+			requestAnimationFrame((e) => {
+				required_input.disabled = true;
+			});
+			this._writeProgress(2,
+			                    Math.max(this._changes.progress_amount, 2),
+				            false);
+		}
+		this._changes.counter = e.path[0].checked;
+	}
+	_changeRequired (e, reset) {
+		let input_value = e.path[0].value;
+		if (input_value === '') {
+			if (reset) {
+				e.path[0].value = this._changes.required_amount;
+			}
+			return;
+		} else {
+			let value = parseInt(input_value);
+			if (value < 1) {
+				if (reset) {
+					e.path[0].value = this._changes.required_amount;
+				}
+				return;
+			}
+			this._changes.required_amount = value;
+			this._writeProgress(value,
+				            Math.max(this._changes.progress_amount),
+				            this._changes.counter);
+			this._changes.required_amount = value;
+		}
+	}
+	_changeProgress (e) {
+		let progress = e.path[0].value;
+		this._changes.progress_amount = progress;
+		this._writeProgress(this._changes.required_amount, progress,
+		                    this._changes.counter);
+	}
+	_incrementProgress (e) {}
+	_decrementProgress (e) {}
+	_changeMeta (e) {}
+	_changeDescription (e) {}
+	cancel () {
+		console.log('Would have reset changes');
+	}
 }
 
 Elements.elements.ProjectsProjectFullEditor = ProjectsProjectFullEditor;
