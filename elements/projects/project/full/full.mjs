f1337434531d2d4760cc344dabaaf5d8dc10fb30
William Chow <431741580derp@gmail.com>
2019-12-24 19:12:59 +1100

    Include dependencies in project-full

--- elements/projects/project/full/full.mjs
+++ elements/projects/project/full/full.mjs
@@ -1,17 +1,18 @@
-export const recommends = ['projects-project-full-display', 'projects-project-full-editor', 'container-rotate'];
+export const recommends = ['projects-project-full-display', 'projects-project-full-editor', 'container-rotate', 'projects-project-selection'];
 export const requires = [];
 
 import {Elements} from '../../../Elements.mjs';
 
 const DISPLAY_SELECTOR = 'elements-projects-project-full-display';
 const EDIT_SELECTOR = 'elements-projects-project-full-editor';
+const DEPEND_SELECTOR = 'elements-projects-project-selection';
 
 /**
  * Full display for a project. Designed to be shown on it's own page
  * @augments Elements.elements.backbone3
  * @memberof Elements.elements
  * @property {Projects.Project} data The Project to show/edit
- * @property {Boolean} editmode Which mode to show
+ * @property {Integer} editmode Which mode to show, 0 = Overview, 1 = Basic editor, 2 = Dependencies editor
  */
 class ProjectsProjectFull extends Elements.elements.backbone3 {
 	constructor () {
@@ -24,15 +25,21 @@ class ProjectsProjectFull extends Elements.elements.backbone3 {
 
 		this._refresh_callback = (changed) => {this._display();};
 		this._data = null;
-		this._editmode = false;
+		this._editmode = 0;
 		this._display_animation = null;
 		this._editor_animation = null;
-		template.querySelector('#edit').addEventListener('click', (e) => {
-			this.change_mode();
+		template.querySelector('#overview').addEventListener('click', (e) => {
+			this.editmode = 0;
+		});
+		template.querySelector('#editor').addEventListener('click', (e) => {
+			this.editmode = 1;
+		});
+		template.querySelector('#depend').addEventListener('click', (e) => {
+			this.editmode = 2;
 		});
 		shadow.appendChild(template);
 		this.applyPriorProperties('data');
-		this.applyPriorProperty('editmode', false);
+		this.applyPriorProperty('editmode', 0);
 	}
 	connectedCallback () {
 		super.connectedCallback();
@@ -70,36 +77,22 @@ class ProjectsProjectFull extends Elements.elements.backbone3 {
 		}
 		this.shadowRoot.querySelector(DISPLAY_SELECTOR).data = value;
 		this.shadowRoot.querySelector(EDIT_SELECTOR).data = value;
+		const depend = this.shadowRoot.querySelector(DEPEND_SELECTOR)
+		depend.contents = value.dependencies;
 		this._display();
 	}
 	get editmode () {
 		return this._editmode;
 	}
 	set editmode (value) {
-		let new_mode = Elements.booleaner(value);
+		let new_mode = parseInt(value);
 		if (this._editmode === new_mode) {return;}
 		this._editmode = new_mode;
 		if (this.attributeInit) {
 			this.setAttribute('editmode', value.toString());
 		}
-		let edit_button = this.shadowQuery('#edit');
-		requestAnimationFrame((e) => {
-			if (new_mode) {
-				edit_button.innerHTML = 'Cancel';
-			} else {
-				edit_button.innerHTML = 'Edit';
-			}
-		});
 		let rotator = this.shadowQuery('elements-container-rotate');
-		if (new_mode) {
-			rotator.current = 's2';
-		} else {
-			rotator.current = 's1';
-		}
-		if (!new_mode) {
-			let editor = this.shadowQuery('elements-projects-project-full-editor');
-			editor.cancel();
-		}
+		rotator.current = 's' + (new_mode + 1).toString();
 	}
 	/**
 	 * Update the project (title) display
@@ -119,11 +112,19 @@ class ProjectsProjectFull extends Elements.elements.backbone3 {
 			title_display.innerHTML = Elements.nameSanitizer(title);
 		});
 	}
+	_accept (e) {
+		let change_set = this.shadowQuery(EDIT_SELECTOR).make_change_set();
+		this._send_change_set(change_set);
+	}
 	/**
-	 * Toggle between edit and display mode
+	 * Send the change_set to the server
+	 * @param  {Projects.ChangeSet}  change_set ChangeSet to send
 	 */
-	change_mode () {
-		this.editmode = !this._editmode;
+	async _send_change_set (change_set) {
+		let result = await Projects.main_project.change_project(change_set);
+		if (!result) {
+			console.log('Failed to contact the server');
+		}
 	}
 }
 
