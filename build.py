28f951973d9c847015795d621fff602b8cc6d696
William Chow <431741580derp@gmail.com>
2020-10-25 05:42:59 +1100

    Add a basic build script

---
+++ build.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+
+def main():
+    subprocess.call(['python3', 'manifest_builder.py'])
+    os.chdir('elements/')
+    subprocess.call('tsc')
+    os.chdir('../')
+
+
+if __name__ == '__main__':
+    main()
