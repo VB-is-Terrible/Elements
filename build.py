cf5fa4227e2148a2c5fce29cbb0930911a2f0d68
William Chow <431741580derp@gmail.com>
2021-02-06 23:54:47 +1100

    Add building the docs to the build process

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ import subprocess
 
 def main():
     subprocess.call(['python3', 'manifest_builder.py'])
+    subprocess.call(['bash', 'build_docs.bash'])
     os.chdir('elements/')
     subprocess.call('tsc')
     os.chdir('../')
