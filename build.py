5c99350f30a726b3e419b2cf9544e3fb23079f74
William Chow <431741580derp@gmail.com>
2021-04-14 23:29:21 +1000

    Switch to the new manifest_builder

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 def main():
-    subprocess.call(['python3', 'manifest_builder.py'])
+    subprocess.call(['python3', 'manifest_builder2.py'])
     subprocess.call(['bash', 'build_docs.bash'])
     os.chdir('elements/')
     subprocess.call('tsc')
