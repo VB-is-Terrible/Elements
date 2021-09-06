7e8f32f6d5d3a26e0fa69619d392dbf651dc40e6
William Chow <431741580derp@gmail.com>
2021-09-06 21:51:13 +1000

    Add multiple build folder support

--- build.py
+++ build.py
@@ -2,14 +2,20 @@
 
 import os
 import subprocess
+import pathlib
+
+
+PROJECTS = ['elements', 'Reader']
 
 
 def main():
     subprocess.call(['python3', 'manifest_builder2.py'])
     subprocess.call(['bash', 'build_docs.bash'])
-    os.chdir('elements/')
-    subprocess.call('tsc')
-    os.chdir('../')
+    cwd = pathlib.Path.cwd()
+    for project in PROJECTS:
+        os.chdir(project)
+        subprocess.call('tsc')
+        os.chdir(cwd)
 
 
 if __name__ == '__main__':
