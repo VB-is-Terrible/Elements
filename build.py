674a89d2e289c9329ca64d28dd5327cdf258e743
William Chow <431741580derp@gmail.com>
2021-09-07 17:42:46 +1000

    Autodetect project folders from tsconfig

--- build.py
+++ build.py
@@ -5,14 +5,19 @@ import subprocess
 import pathlib
 
 
-PROJECTS = ['elements', 'Reader']
-
-
 def main():
     subprocess.call(['python3', 'manifest_builder2.py'])
     subprocess.call(['bash', 'build_docs.bash'])
     cwd = pathlib.Path.cwd()
-    for project in PROJECTS:
+    folders = []
+    for inode in os.listdir():
+        if os.path.isdir(inode):
+            folders.append(inode)
+    projects = []
+    for folder in folders:
+        if os.path.isfile(os.path.join(folder, 'tsconfig.json')):
+            projects.append(folder)
+    for project in projects:
         os.chdir(project)
         subprocess.call('tsc')
         os.chdir(cwd)
