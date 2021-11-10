d93063078e2554e1206a5849013f059c0b0da3ab
William Chow <431741580derp@gmail.com>
2021-11-10 14:21:11 +1100

    Start getting tags working

--- elements/projects3/project/creator/creator.ts
+++ elements/projects3/project/creator/creator.ts
@@ -3,7 +3,7 @@ const requires: Array<string> = [];
 
 import {Elements} from '../../../elements_core.js';
 import {backbone4} from '../../../elements_backbone.js';
-import {CustomComposedEvent} from '../../../elements_helper.js';
+import {CustomComposedEvent, removeChildren} from '../../../elements_helper.js';
 import {FormWrapper} from '../../../container/form/Common.js';
 import type {ProjectObj, id} from '../../Common/Common.js';
 
@@ -39,9 +39,13 @@ export class AcceptDetail implements ProjectObj {
  * @memberof Elements.elements
  */
 export class Projects3ProjectCreator extends FormWrapper(backbone4) {
+	#project_id = -1;
 	#name: HTMLInputElement;
 	#desc: HTMLTextAreaElement;
 	#warn: HTMLImageElement;
+	#tags: HTMLDivElement;
+	#tag_input: HTMLInputElement;
+	#tags_store: string[] = [];
 	constructor() {
 		super();
 
@@ -50,6 +54,17 @@ export class Projects3ProjectCreator extends FormWrapper(backbone4) {
 		this.#name = template.querySelector('#project_title') as HTMLInputElement;
 		this.#desc = template.querySelector('#proj_desc') as HTMLTextAreaElement;
 		this.#warn = template.querySelector('img.warn') as HTMLImageElement;
+		this.#tags = template.querySelector('div.tag_holder') as HTMLDivElement;
+		this.#tag_input = template.querySelector('#tag_input') as HTMLInputElement;
+		this.#tag_input.addEventListener('keydown', (e) => {
+			if (e.key === 'Enter') {
+				this.#tagInsert();
+			}
+		});
+		const tag_confirm = template.querySelector('div.tag_input button') as HTMLButtonElement;
+		tag_confirm.addEventListener('click', () => {
+			this.#tagInsert();
+		})
 		this.#name.addEventListener('blur', () => {
 			if (this.#name.value === '') {
 				requestAnimationFrame(() => {
@@ -60,6 +75,7 @@ export class Projects3ProjectCreator extends FormWrapper(backbone4) {
 
 		//Fancy code goes here
 		shadow.appendChild(template);
+		// this.reset();
 	}
 	static get observedAttributes() {
 		return [];
@@ -69,7 +85,7 @@ export class Projects3ProjectCreator extends FormWrapper(backbone4) {
 			return;
 		}
 		const detail = new AcceptDetail(
-			-1,
+			this.#project_id,
 			this.#name.value,
 			this.#desc.value,
 			[],
@@ -84,6 +100,52 @@ export class Projects3ProjectCreator extends FormWrapper(backbone4) {
 		requestAnimationFrame(() => {
 			this.#warn.style.display = 'none';
 		});
+		this.#tags_store = [];
+		this.setTags(['this', 'is', 'a', 'test', 'nothing but a really', 'long test phrase', 'to test when the tags', 'should overflow', 'and it turns out that', 'this list of strings is not', 'big enough']);
+
+	}
+	private setTags(tags: string[]) {
+		removeChildren(this.#tags);
+		this.#tags_store = tags;
+		const tag_buttons: HTMLElement[] = [];
+		for (const tag of tags) {
+			tag_buttons.push(this.#createTag(tag));
+		}
+		requestAnimationFrame(() => {
+			for (const tag of tag_buttons) {
+				this.#tags.append(tag);
+			}
+			// this.#tags.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		});
+	}
+	#createTag(tag: string) {
+		const result = document.createElement('button');
+		result.className = 'tag_remove';
+		const name = document.createElement('span');
+		name.className = 'tag_name';
+		name.textContent = tag;
+		result.append(name);
+		result.addEventListener('click', () => {
+			this.#removeTag(tag, result);
+		})
+		return result;
+	}
+	#removeTag(tag: string, button: HTMLButtonElement) {
+		button.remove();
+		const index = this.#tags_store.indexOf(tag);
+		this.#tags_store.splice(index);
+	}
+	setEdit(project: ProjectObj) {
+		this.#project_id = project.id;
+		this.#name.value = project.name
+		this.#desc.value = project.desc;
+		this.setTags(project.tags);
+	}
+	#tagInsert() {
+		const new_tag = this.#tag_input.value;
+		this.#tags_store.push(new_tag);
+		const button = this.#createTag(new_tag);
+		this.#tags.append(button);
 	}
 }
 
