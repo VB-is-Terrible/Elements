6fec0141585d3373934212cfe27533689995521e
William Chow <431741580derp@gmail.com>
2022-06-24 23:18:05 +1000

    Start wiring up tag displays

--- Projects/projects3project.ts
+++ Projects/projects3project.ts
@@ -5,7 +5,7 @@ import type {ContainerStacked} from '../elements/container/stacked/stacked.js';
 import type {Toaster} from '../elements/toaster/toaster.js';
 import {remote as ROOT_LOCATION} from './projects3base.js'
 import type {Projects3Tagbar} from '../elements/projects3/tagbar/tagbar.js';
-
+import {removeChildren} from '../elements/elements_helper.js';
 
 type ProjectData = {
 	project: ProjectObj,
@@ -23,15 +23,20 @@ Elements.get('container-stacked', 'projects3-tagbar');
 const project_link = document.querySelector('a.project_link') as HTMLAnchorElement;
 const meta_title = project_link.querySelector('p.header') as HTMLParagraphElement;
 const title = document.querySelector('p.group_title') as HTMLParagraphElement;
-const desc = document.querySelector('textarea.group_desc') as HTMLTextAreaElement;
+const group_desc = document.querySelector('textarea.group_desc') as HTMLTextAreaElement;
 const title_editor = document.querySelector('#group_name_edit') as HTMLInputElement;
 const button_stack = document.querySelector('div.bottom_buttons > elements-container-stacked') as ContainerStacked;
 const title_stack = document.querySelectorAll('.title_stack_holder > .title') as NodeListOf<HTMLDivElement>;
 const toaster = document.querySelector('#toaster') as Toaster;
 const tagbar = document.querySelector('elements-projects3-tagbar') as Projects3Tagbar;
+const tags_desc = document.querySelector('.tags_desc') as HTMLDivElement;
+const current_tags = document.querySelector('.current_tags') as HTMLDivElement;
+const removed_tags = document.querySelector('.removed_tags') as HTMLDivElement;
+
 
 
 export let system: Project;
+let system_tags: Record<string, string> = {};
 
 class PageState {
 	#tags = false;
@@ -45,12 +50,16 @@ class PageState {
 				requestAnimationFrame(() => {
 					title_stack[1].style.display = 'none';
 					title_stack[2].style.display = 'grid';
+					tags_desc.style.display = 'grid';
+					group_desc.style.display = 'none';
 					tagbar.focus();
 				});
 			} else {
 				requestAnimationFrame(() => {
 					title_stack[1].style.display = 'grid';
 					title_stack[2].style.display = 'none';
+					tags_desc.style.display = 'none';
+					group_desc.style.display = 'initial';
 					title_editor.focus();
 				});
 			}
@@ -62,17 +71,21 @@ class PageState {
 				requestAnimationFrame(() => {
 					title_stack[0].style.display = 'none';
 					title_stack[2].style.display = 'grid';
+					tags_desc.style.display = 'grid';
+					group_desc.style.display = 'none';
 				});
 			} else {
 				requestAnimationFrame(() => {
 					title_stack[0].style.display = 'grid';
 					title_stack[2].style.display = 'none';
+					tags_desc.style.display = 'none';
+					group_desc.style.display = 'initial';
 				});
 			}
 		}
 		button_stack.current = !this.#edit ? 's1' : 's2';
 		title_editor.disabled = !this.#edit;
-		desc.disabled = !this.#edit;
+		group_desc.disabled = !this.#edit;
 		tagbar.disabled = !this.#edit;
 	}
 	get tags() {
@@ -133,6 +146,11 @@ export const main = () => {
 		});
 
 	}
+	tagbar.addEventListener('accept', (e) => {
+		addTag((e as CustomEvent<string>).detail);
+	});
+
+
 	(async () => {
 		load_remote();
 	})();
@@ -158,17 +176,40 @@ const load = (system: Project, owner: number, owner_name: string, tags: Record<s
 			meta_title.textContent = owner_name;
 		});
 	}
+	system_tags = tags;
 	reset();
 };
-
 
 const reset = () => {
 	PAGE_STATE.edit = false;
 	title.textContent = system.name;
 	title_editor.value = system.name;
-	desc.value = system.desc;
+	group_desc.value = system.desc;
+	removeChildren(current_tags);
+	removeChildren(removed_tags);
+	for (const tag of system.tags) {
+		const tag_button = createTagDisplay(tag, system_tags[tag]);
+		requestAnimationFrame(() => {
+			current_tags.append(tag_button);
+		});
+	}
 };
 
+const createTagDisplay = (tag: string, color: string = '') => {
+	const result = document.createElement('button');
+	result.className = 'tag';
+	const name = document.createElement('span');
+	name.className = 'tag_name';
+	name.textContent = tag;
+	result.append(name);
+	if (color !== '') {
+		result.style.backgroundColor = color;
+	}
+	result.addEventListener('click', () => {
+		removeTag(tag, result);
+	});
+	return result;
+}
 
 
 // const modifyNetworkGroup = async (title: string, desc: string) => {
@@ -198,3 +239,12 @@ const reset = () => {
 // 	system.desc = desc;
 // 	reset();
 // }
+
+const addTag = (tag: string) => {
+	alert(tag);
+};
+
+const removeTag = (tag: string, button: HTMLButtonElement) => {
+	alert(tag);
+	button.remove();
+};
